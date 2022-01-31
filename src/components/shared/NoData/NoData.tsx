5d147c05900bc1b730255faf466e703266e7454d
rahulh11041996 <rahullmcstece@gmail.com>
2022-01-31 18:38:23 +0530

    no data

--- src/components/shared/NoData/NoData.tsx
+++ src/components/shared/NoData/NoData.tsx
@@ -3,9 +3,11 @@ import { DICTIONARY } from '../../../assets/data/dict';
 
 export default function NoData() {
   return (
-    <div className="no_details">
-      <i className="fas fa-sad-tear"></i>
-      <h1>{DICTIONARY.NO_TODO_LIST}</h1>
+    <div className="no_details gap-6 flex pt-16 items-center justify-center flex-col">
+      <i className="fas fa-sad-tear text-7xl text-greyprimary opacity-30"></i>
+      <h1 className="text-2xl text-greyprimary font-semibold opacity-60 italic">
+        {DICTIONARY.NO_TODO_LIST}
+      </h1>
     </div>
   );
 }
